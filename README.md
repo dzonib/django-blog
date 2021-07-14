aadd255cb12190abf7ad1ccc25d1b84930011e4f
bosnjaknikola <nikola.bosnjak@wayseven.com>
2021-07-14 17:46:12 +0200

    adding base templates and styles

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# django-blog
-blog using pure django
+## Create a project 
+`django-admin startproject my_site`
+
+## Create a app
+`python manage.py startapp blog`
+
+## Create a virtual environment
+`virtualenv my_env`
+
+Start venv, install django, create requirements.txt,
+pip freeze in root,
+add dependencies to requirements.txt
+
+pip install -r requirements.txt
+
+to install deps
+
+## Start v-env and runserver
+`cd my_env/scripts && activate && cd ../../ && python manage.py runserver`
--- my_site/settings.py
+++ my_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
